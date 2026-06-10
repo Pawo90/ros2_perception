205d782ed342764b19dfb4348fb4ddfae9534e8a
pawo90 <pawelp1990@gmail.com>
2026-06-10 21:52:05 +0200

    Update README and add img processing node

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # ros2_perception
+
+A collection of ROS2 perception nodes built with OpenCV and Python.
+Each package demonstrates a different concept in robot perception — from basic image processing to object detection.
+
+## Packages
+| Package | Description |
+|---|---|
+| `image_processing` | Image subscriber, cv_bridge, basic OpenCV operations |
+
+## 01 Image Processing
+Basic ROS2 image pipeline — subscribing to a camera topic and processing frames with OpenCV.
+
+### RUN
+#### Terminal 1 - Start the camera publisher:
+```bash
+ros2 run image_tools cam2image
+```
+
+#### Terminal 2 - Start the image subscriber node:
+```bash
+ros2 run image_processing image_node
+```
+
+#### Topics
+| Topic | Type | Description |
+| --- | --- | --- |
+| '/image' | 'sensor_msgs/Image' | Raw image from camera
--- src/image_processing/image_processing/image_node.py
+++ src/image_processing/image_processing/image_node.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+
+"""
+ROS2 IMAGE SUBSCIRBER NODE
+
+This node subscribes to the '/image' topic and display image.
+"""
+
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
@@ -21,21 +28,21 @@ class ImageSubscriber(Node):
         self.bridge = CvBridge()
 
 
-def callback_image(self, msg: Image):
-    try:
-        # Conver ROS2 Image message to OpenCV image
-        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+    def callback_image(self, msg: Image):
+        try:
+            # Conver ROS2 Image message to OpenCV image
+            cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
 
-        # --- OPTIONAL ---
-        # Process the image (conver to grayscale, rotate, etc)
-        gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+            # --- OPTIONAL ---
+            # Process the image (conver to grayscale, rotate, etc)
+            gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
 
-        # Display the image
-        cv2.imshow("Processed Image", cv_image)
-        cv2.waitKey(1)
+            # Display the image
+            cv2.imshow("Processed Image", cv_image)
+            cv2.waitKey(1)
 
-    except Exception as e:
-         self.get_logger().error(f"Error processing image: {e}")
+        except Exception as e:
+            self.get_logger().error(f"Error processing image: {e}")
 
 
 def main(args=None):
